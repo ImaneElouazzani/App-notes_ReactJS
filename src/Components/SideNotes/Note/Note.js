7e3fd9edca35e8060f2e5419d6d9722e163fae33
Imane Elouazzani <denjala.smiley@gmail.com>
2022-02-10 22:45:01 +0100

    supprimer les notes

--- src/Components/SideNotes/Note/Note.js
+++ src/Components/SideNotes/Note/Note.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import './Note.css'
+import delIcon from './remove.svg'
+import edit from './edit.svg'
+import {Link} from 'react-router-dom'
+import {useDispatch} from 'react-redux'
 
-export default function Note() {
-  return <div>
-    Note
-  </div>;
+export default function Note(props) {
+  //console.log(props)
+
+  const dispatch = useDispatch()
+
+  const deleteNote = () => {
+    dispatch({
+      type: 'DELETENOTE',
+      payload: props.id
+    })
+  }
+
+  return (
+  <li className='txt-note-prev'>
+    <div className='bloc-note-left'>
+      <p>{props.title}</p>
+      <p>{props.subtitle}</p>
+    </div>
+    <div className='bloc-note-right'>
+      <button
+        onClick={deleteNote}
+      >
+        <img src={delIcon} alt='delete icon'/>
+      </button>
+      <button>
+        <img src={edit} alt='edit icon'/>
+      </button>
+    </div>
+  </li>)
 }

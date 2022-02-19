b19833751cb6eefc3083cac3c1d5d6ae5f07b455
Imane Elouazzani <denjala.smiley@gmail.com>
2022-02-19 16:09:26 +0100

    ajouter, filtrer et supprimer les notes

--- src/Components/SideNotes/SideNotes.js
+++ src/Components/SideNotes/SideNotes.js
@@ -14,12 +14,19 @@ export default function SideNotes() {
   }, [notes])
 
   const preventForm = e => e.preventDefault()
-  console.log(notesList)
+  
+  const handleFilter = e => {
+    const stateCopy = [...notes]
+    const filteredArr = stateCopy.filter((item) => item.title.toLowerCase().includes(e.target.value.toLowerCase()))
+    setNotesList(filteredArr)
+  }
 
   return <div className='notes-display'>
       <h2>Mes Notes</h2>
       <form onSubmit={preventForm}>
-        <input type="text"
+        <input
+          onChange={handleFilter} 
+          type="text"
           id='search-notes'
           placeholder='Rechercher'
         />
